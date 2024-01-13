5cb9103dde18de43b74af44da17fb2ad0657ac77
Aparna Mishra <132871197+Aparna12Mishra@users.noreply.github.com>
2024-01-13 13:48:13 +0530

    Time: 48 ms (97.15%) | Memory: 41.8 MB (84.05%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -3,17 +3,14 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
-  // Wait for both promises to resolve and retrieve their values
-  const [value1, value2] = await Promise.all([promise1, promise2]);
+var addTwoPromises = async function (promise1, promise2) {
+  const res1 = await promise1
+  const res2 = await promise2
 
-  // Return a new promise that resolves with the sum of the values
-  return value1 + value2;
-};
+  return res1 + res2
+}
 
-// // Example usage:
-// var promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20));
-// var promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60));
-
-// addTwoPromises(promise1, promise2)
-//   .then(console.log); // Output: 7
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
