62d4f74ee4407ff7f41c8518b0b1163284b6edbc
Aparna Mishra <132871197+Aparna12Mishra@users.noreply.github.com>
2024-01-14 15:09:45 +0530

    Time: 56 ms (41.72%) | Memory: 44.3 MB (14.07%) - LeetSync

--- 2805-array-wrapper/array-wrapper.js
+++ 2805-array-wrapper/array-wrapper.js
@@ -1,22 +1,29 @@
 /**
  * @param {number[]} nums
+ * @return {void}
  */
-
-const ArrayWrapper = function(nums) {
+var ArrayWrapper = function(nums) {
     this.nums = nums;
 };
 
+/**
+ * @return {number}
+ */
 ArrayWrapper.prototype.valueOf = function() {
-    let sum = 0;
-    for (let i = 0; i < this.nums.length; i++) {
-        sum += this.nums[i];
-    }
-    return sum;
-};
+    
 
+    return this.nums.reduce((p,c) => p+c,0)
+}
+
+/**
+ * @return {string}
+ */
 ArrayWrapper.prototype.toString = function() {
-    return "[" + this.nums.join(",") + "]";
-};
+
+    let string = this.nums.toString()
+    return `[${string}]`
+    
+}
 
 /**
  * const obj1 = new ArrayWrapper([1,2]);
