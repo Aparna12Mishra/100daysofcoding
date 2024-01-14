222fa3d0377de3f1cf34852d5be1646abbd72108
Aparna Mishra <132871197+Aparna12Mishra@users.noreply.github.com>
2024-01-14 15:08:46 +0530

    Time: 83 ms (68.13%) | Memory: 51.8 MB (75.50%) - LeetSync

--- 2804-compact-object/compact-object.js
+++ 2804-compact-object/compact-object.js
@@ -1,22 +1,54 @@
-var compactObject = function(obj) {
-    if (typeof obj === 'object') {
-        if (Array.isArray(obj)) {
-            const tempArray = [];
-            for (let index = 0; index < obj.length; index++) {
-                if (Boolean(obj[index])) {
-                    tempArray.push(compactObject(obj[index]));
-                }
+// takes an object and returns only obj with keys having truthy values only
+function compactObject(param) {
+    if (param === null) return null;
+
+    // if the param is an array
+    if (Array.isArray(param)) {
+        // create a newArray
+        const compactedArr = [];
+
+        // loop through the array param
+        for (let i = 0; i < param.length; i++) {
+            // isolate the value at each index position
+            const currParam = param[i];
+
+            // check if currParam is null, array, object, or not by running it through this function again
+            // accounts for nested objects
+            const subResult = compactObject(currParam);
+
+            // if subResult is true, add it to the compactedArr
+            if (subResult) {
+                compactedArr.push(subResult);
             }
-            return tempArray;
-        } else {
-            const tempObject = {};
-            for (const key in obj) {
-                if (Boolean(obj[key])) {
-                    tempObject[key] = compactObject(obj[key]);
-                }
-            }
-            return tempObject;
+        }
+
+        return compactedArr;
+    }
+
+    // at this point, function has checked if param is null or an array. If it's neither and it's also NOT an object
+    if (typeof param !== 'object') {
+        // then the param itself is returned wherever compactObject(x) has been called
+        return param;
+    }
+
+
+    // create a new object
+    const compactObj = {};
+
+    // loop through keys in object
+    for (const key in param) {
+        const currParam = param[key];
+
+        // check if currParam is null, array, object, or not
+        const subResult = compactObject(currParam);
+
+        // if subResult is true in this loop
+        if (subResult) {
+            // add compactObj {"[key]" : "subResult"} 
+            compactObj[key] = subResult;
         }
     }
-    return obj;
-};
+
+    return compactObj;
+
+}
