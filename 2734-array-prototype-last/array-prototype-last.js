019caf3c26256bfaefe1d9c3cb8056ca950a4759
Aparna Mishra <132871197+Aparna12Mishra@users.noreply.github.com>
2024-01-14 15:06:46 +0530

    Time: 47 ms (76.19%) | Memory: 41.8 MB (45.37%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -1,3 +1,12 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
 Array.prototype.last = function() {
-  return this.length ? this.slice(-1)[0] : -1;
+    if (this.length===0){
+        return -1
+    }
+    return this[this.length-1]
 };
+
+const arr = [1, 2, 3];
+arr.last(); // 3
